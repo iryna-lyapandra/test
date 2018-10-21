f8eb1c095f15f3c2cc79520aad1a14c158e445d3
Andriy Lyapandra <lyapandra.andriy.s@gmail.com>
2018-10-21 23:53:50 +0300

    added methods add subtract

--- src/ClassA.java
+++ src/ClassA.java
@@ -2,4 +2,13 @@
  * Created by a on 10/21/2018.
  */
 public class ClassA {
+    int add (int a, int b){
+        return a+b;
+    }
+
+    int subtract (int a, int b){
+        return a-b;
+    }
+
+
 }
--- src/I.java
+++ src/I.java
@@ -3,4 +3,6 @@
  */
 public interface I {
     void i();
+
+
 }
